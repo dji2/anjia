bac1e13b71810441dfea3cf9d857568dccbf0bfd
Tkwkrystal <894766704@qq.com>
2017-09-14 20:52:22 +0800

    修改house-detail 修改houses 业主模块修改 背景图上传

--- anjiaApp/src/app/services/positions.service.ts
+++ anjiaApp/src/app/services/positions.service.ts
@@ -5,7 +5,7 @@ import {HttpClient} from '@angular/common/http';
 @Injectable()
 export class PositionsService {
 
-  url:string='http://10.40.4.20:3000/houses';
+  url:string='http://10.40.4.64:3000/houses';
   constructor(
     private http:HttpClient
   ) {
