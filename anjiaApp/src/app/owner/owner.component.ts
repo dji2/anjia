89702b12bfcacf95bfbad9320b5c27aa449c89c0
Tkwkrystal <894766704@qq.com>
2017-09-15 15:44:34 +0800

    owner修改 表单验证

--- anjiaApp/src/app/owner/owner.component.ts
+++ anjiaApp/src/app/owner/owner.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from './../services/users.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-owner',
   templateUrl: './owner.component.html',
-  styleUrls: ['./owner.component.css']
+  styleUrls: ['./owner.component.css'],
+  providers:[UsersService],
 })
 export class OwnerComponent implements OnInit {
 
-  constructor() { }
+  owner_res:string;
+   formstatus:boolean = false;
+  constructor(
+    private userSer:UsersService,
+    private router:Router
+  ) { }
 
   ngOnInit() {
   }
+  toRelease(ownerForm){
+    let that=this;
+    that.userSer.login(ownerForm.form.value,function (result) {
+      // 弹出接收后台数据数值
+      alert(result.stateCode);
 
+      if(result.stateCode==1){
+        that.router.navigate(['/index']);
+      }else {
+        alert(result.stateCode);
+        that.owner_res='提交失败，请稍后再试';
+      }
+    })
+  }
+  setCommit():void{
+    this.formstatus = !this.formstatus;
+    console.log(this.formstatus)
+  }
 }
 
