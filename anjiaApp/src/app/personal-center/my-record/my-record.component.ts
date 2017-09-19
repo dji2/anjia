d83cd246dd7d827e53b200fb659d148990be2c8f
Jdsoon <764922900@qq.com>
2017-09-19 21:26:17 +0800

    个人中心 访问记录

--- anjiaApp/src/app/personal-center/my-record/my-record.component.ts
+++ anjiaApp/src/app/personal-center/my-record/my-record.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from './../../services/users.service';
+import { LocalStorageService } from './../../services/local-storage.service';
 
 @Component({
   selector: 'app-my-record',
   templateUrl: './my-record.component.html',
-  styleUrls: ['./my-record.component.css']
+  styleUrls: ['./my-record.component.css'],
+  providers:[UsersService],
 })
 export class MyRecordComponent implements OnInit {
+  houses:any;
+  constructor(
+    private userSer:UsersService,
+    private localStorage:LocalStorageService
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+    let that = this;
+
+
+    that.userSer.getRecord({"userId":2},function (result) {
+      that.houses = result;
+
+      console.log(result);
+    })
   }
 
 }
