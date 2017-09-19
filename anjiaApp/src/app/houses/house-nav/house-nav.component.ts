f930a9f94dfc7f8de8a020b078cd5545303b716d
Tkwkrystal <894766704@qq.com>
2017-09-19 18:18:04 +0800

    house-nav-list 修改

--- anjiaApp/src/app/houses/house-nav/house-nav.component.ts
+++ anjiaApp/src/app/houses/house-nav/house-nav.component.ts
@@ -7,28 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HouseNavComponent implements OnInit {
 
+  flag_number='quyu';
+  flag_number1='zujin';
+  flag_number2='mianji';
+  flag_number3=6;
   constructor() { }
 
   ngOnInit() {
   }
   _quyu:string;
 
+
   do(s){
     this._quyu=s;
+    this.flag_number=s;
   }
   _zujin:string;
 
   do1(s){
     this._zujin=s;
+    this.flag_number1=s;
   }
   _mianji:string;
 
   do2(s){
     this._mianji=s;
+    this.flag_number2=s;
   }
   _zujinxiao:string;
 
   do3(s){
     this._zujinxiao=s;
+    this.flag_number3=s;
   }
 }
--- anjiaApp/src/app/pipes/zujinxiao.pipe.ts
+++ anjiaApp/src/app/pipes/zujinxiao.pipe.ts
@@ -6,31 +6,38 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ZujinxiaoPipe implements PipeTransform {
 
   transform(houses: any, args?: any): any {
+    // console.log(houses);
     if(args==1){
      var new_houses=houses;
       new_houses.sort(function (houseA,houseB) {
-          return parseInt( houseA.housePrice) - parseInt( houseB.housePrice);
+          return  houseA.housePrice - houseB.housePrice;
       })
-
       return new_houses;
     }else if(args==2) {
       var new_houses=houses;
       new_houses.sort(function (houseA,houseB) {
-        return parseInt( houseB.housePrice)-parseInt( houseA.housePrice) ;
+        return houseB.housePrice - houseA.housePrice ;
       })
       return new_houses;
     }else if(args==3) {
       var new_houses=houses;
       new_houses.sort(function (houseA,houseB) {
-        return parseInt( houseA.area)-parseInt( houseB.area) ;
+        return  houseA.area - houseB.area ;
       })
       return new_houses;
     }else if(args==4) {
       var new_houses=houses;
       new_houses.sort(function (houseA,houseB) {
-        return parseInt( houseB.area)-parseInt( houseA.area) ;
+        return  houseB.area - houseA.area ;
       })
       return new_houses;
+    }else if(args==5) {
+      var new_houses=houses;
+      new_houses.sort(function (houseA,houseB) {
+        return houseB.publishTime - houseA.publishTime ;
+      })
+      return new_houses;
+
     }else{
       return houses;
     }
