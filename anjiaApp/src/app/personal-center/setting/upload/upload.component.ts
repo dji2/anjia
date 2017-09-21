966024ab9efd03677da1e82813fec80357920eb2
Jdsoon <764922900@qq.com>
2017-09-21 14:46:34 +0800

    上传头像

--- anjiaApp/src/app/personal-center/setting/upload/upload.component.ts
+++ anjiaApp/src/app/personal-center/setting/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from './../../../services/local-storage.service';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  userId:any;
+  constructor(
+    private localStorage:LocalStorageService
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+    this.userId = this.localStorage.get('userId');
+    console.log(this.userId)
   }
 
 }
