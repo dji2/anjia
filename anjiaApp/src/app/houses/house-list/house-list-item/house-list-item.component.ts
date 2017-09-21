a5b15763e2221eb13ff74ad9d78e65e04e2ef33a
Jdsoon <764922900@qq.com>
2017-09-21 09:45:59 +0800

    添加house-list-item模块

---
+++ anjiaApp/src/app/houses/house-list/house-list-item/house-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-house-list-item',
+  templateUrl: './house-list-item.component.html',
+  styleUrls: ['./house-list-item.component.css']
+})
+export class HouseListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
