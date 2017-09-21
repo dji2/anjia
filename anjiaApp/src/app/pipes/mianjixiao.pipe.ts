19619e547fd8d341c7a3756b546e30b69c9fa2eb
Tkwkrystal <894766704@qq.com>
2017-09-21 14:28:32 +0800

    分页

--- anjiaApp/src/app/pipes/mianjixiao.pipe.ts
+++ anjiaApp/src/app/pipes/mianjixiao.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MianjixiaoPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(houses: any, args?: any): any {
+    // console.log(args)
+    // console.log(houses)
+    var new_houses = houses.slice(10*(args-1),10*(args)-1);
+    return new_houses;
+    // if(args){
+    //   var new_houses=houses.filter(function (house,index) {
+    //     house = houses.slice(10*(args-1),10*(args)-1)
+    //       return house;
+    //   });
+    //   return new_houses;
+    // }else{
+    //   return houses;
+    // }
   }
 
 }
