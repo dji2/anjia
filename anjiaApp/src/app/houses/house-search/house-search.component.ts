ad9d3b39185445722c73b2b6161afcf07ac44a93
Jdsoon <764922900@qq.com>
2017-09-25 16:27:58 +0800

    首页搜索  传值到search模块搜索

--- anjiaApp/src/app/houses/house-search/house-search.component.ts
+++ anjiaApp/src/app/houses/house-search/house-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-house-search',
@@ -7,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HouseSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+
+  ) { }
 
   _stext:string;
 
   ngOnInit() {
+    this._stext=this.route.snapshot.paramMap.get('searchText');
+
+    alert(this._stext);
+    this.search(this._stext);
   }
   search(s){
     this._stext=s;
