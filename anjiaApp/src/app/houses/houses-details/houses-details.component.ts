39a21bbd4644db1e58bb6b4fd6962a9ee2bec97f
github-bian <534893106@qq.com>
2017-09-26 11:17:23 +0800

    添加了地图

--- anjiaApp/src/app/houses/houses-details/houses-details.component.ts
+++ anjiaApp/src/app/houses/houses-details/houses-details.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute, ParamMap} from '@angular/router';
 import {PositionsService} from './../../services/positions.service';
 import { LocalStorageService } from './../../services/local-storage.service';
 declare var $:any;
+declare var AMap:any;
+
+
 @Component({
   selector: 'app-houses-details',
   templateUrl: './houses-details.component.html',
@@ -30,6 +33,48 @@ export class HousesDetailsComponent implements OnInit {
 
   ngOnInit() {
 
+    var map = new AMap.Map('container', {
+      resizeEnable: true,
+      zoom: 12,
+      center: [118.756376, 32.052573]
+    });
+//地图内容
+    map.setFeatures(['bg', 'building', 'road', 'point']);
+//地图空间
+    AMap.plugin(['AMap.ToolBar', 'AMap.Scale', 'AMap.MapType'],
+      function () {
+        map.addControl(new AMap.ToolBar());
+
+        map.addControl(new AMap.Scale());
+      });
+//覆盖物
+    var marker = new AMap.Marker({
+      position: [118.756376, 32.052573]
+    });
+    marker.setMap(map);
+    var circle = new AMap.Circle({
+      center: [118.756376, 32.052573],
+      radius: 100,
+      fillOpacity: 0.2,
+      strokeWeight: 1
+    })
+    circle.setMap(map);
+//自定义窗体
+    var infowindow;
+    var infoWindowContent = '<div class="infowindow-content"><h3>欢迎下载本地图插件</h3><p>基于高德地图API</p><span style="color:red;font-size:12px;">(此处文字可修改)</span></div>';
+    map.plugin('AMap.AdvancedInfoWindow', function () {
+      infowindow = new AMap.AdvancedInfoWindow({
+        panel: 'panel',
+        placeSearch: true,
+        asOrigin: true,
+        asDestination: true,
+        content: infoWindowContent
+      });
+      infowindow.open(map, [118.756376, 32.052573]);
+    });
+
+
+
 
     $(window).scroll(function() {
       var scrolltop = $(this).scrollTop();
@@ -39,10 +84,6 @@ export class HousesDetailsComponent implements OnInit {
         $("#yezhuright").removeClass("yezhuright-after");
       }
     })
-
-
-
-
     this.houseId=this.route.snapshot.paramMap.get('id');
     let that=this;
     that.ho.getHouseById(this.houseId,function (house) {
