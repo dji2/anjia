6296ac973ac96f8ec9c7db7273d1cd8bdc269c90
github-bian <534893106@qq.com>
2017-09-26 13:40:08 +0800

    添加了计算器  y提示 有错误

--- anjiaApp/src/app/index/index.component.ts
+++ anjiaApp/src/app/index/index.component.ts
@@ -38,6 +38,72 @@ export class IndexComponent implements OnInit,AfterContentInit {
 
   ngOnInit() {
 
+
+
+      var $td=$("td");
+
+      $td.each(function(){
+        $(this).click(function(){
+          var Text=$("#input").text().trim();
+          $("#input").append($(this).text());
+          switch ($(this).text()){
+            case "清除":
+              $("#input").text("");
+              break;
+            case "D":
+              $("#input").text(Text.substr(0,Text.length-1));
+              break;
+            case "=":
+            function  compute(content){
+              var index=content.lastIndexOf("(");
+              if(index>-1){
+                var nextIndex=content.indexOf(")",index);
+                if(nextIndex>-1){
+
+                  var result=compute(content.substring(index+1,nextIndex));
+                  return    compute(content.substring(0,index)+(""+result)+content.substring(nextIndex+1))
+                }
+
+              }
+              index=content.indexOf("+");
+              if(index>-1){
+                return compute(content.substring(0,index))+compute(content.substring(index+1));
+              }
+              index=content.lastIndexOf("-");
+              if(index>-1){
+                return compute(content.substring(0,index))-compute(content.substring(index+1));
+              }
+
+              index=content.indexOf("*");
+              if(index>-1){
+                return compute(content.substring(0,index))*compute(content.substring(index+1));
+              }
+              index=content.lastIndexOf("/");
+              if(index>-1){
+                return compute(content.substring(0,index))/compute(content.substring(index+1));
+              }
+              if(content==""){
+                return 0;
+              }else{
+                return Number(content);
+              }
+            }
+              $("#input").text(compute(Text));
+          }
+        })
+
+      });
+
+
+
+
+
+
+
+
+
+
+
     // let map = newAMap.Map('container');
     // map.plugin('AMap.Geolocation',()=>{
     //   let geolocation = newAMap.Geolocation({
@@ -53,63 +119,6 @@ export class IndexComponent implements OnInit,AfterContentInit {
     // });
 
 
-    // $(function(){
-    //
-    //   var $td=$("td");
-    //
-    //   $td.each(function(){
-    //     $(this).click(function(){
-    //       var Text=$("#input").text().trim();
-    //       $("#input").append($(this).text());
-    //       switch ($(this).text()){
-    //         case "清除":
-    //           $("#input").text("");
-    //           break;
-    //         case "D":
-    //           $("#input").text(Text.substr(0,Text.length-1));
-    //           break;
-    //         case "=":
-    //         function  compute(content){
-    //           var index=content.lastIndexOf("(");
-    //           if(index>-1){
-    //             var nextIndex=content.indexOf(")",index);
-    //             if(nextIndex>-1){
-    //
-    //               var result=compute(content.substring(index+1,nextIndex));
-    //               return    compute(content.substring(0,index)+(""+result)+content.substring(nextIndex+1));
-    //             }
-    //
-    //           }
-    //           index=content.indexOf("+");
-    //           if(index>-1){
-    //             return compute(content.substring(0,index))+compute(content.substring(index+1));
-    //           }
-    //           index=content.lastIndexOf("-");
-    //           if(index>-1){
-    //             return compute(content.substring(0,index))-compute(content.substring(index+1));
-    //           }
-    //           //如果返回的content为空,则返回0
-    //           index=content.indexOf("*");
-    //           if(index>-1){
-    //             return compute(content.substring(0,index))*compute(content.substring(index+1));
-    //           }
-    //           index=content.lastIndexOf("/");
-    //           if(index>-1){
-    //             return compute(content.substring(0,index))/compute(content.substring(index+1));
-    //           }
-    //           if(content==""){
-    //             return 0;
-    //           }else{
-    //
-    //             return Number(content);
-    //           }
-    //         }
-    //           $("#input").text(compute(Text));
-    //       }
-    //     })
-    //
-    //   });
-    // });
 
 
 
@@ -209,7 +218,6 @@ export class IndexComponent implements OnInit,AfterContentInit {
       that.islogin=0;
     }
   }
-
 
 
 
