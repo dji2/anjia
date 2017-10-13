82ac948b2ca4c6312680706ba6edd436d215d7c5
Jdsoon <764922900@qq.com>
2017-10-13 14:22:00 +0800

    修改获取远程服务器后台接口

--- anjiaApp/src/app/services/positions.service.ts
+++ anjiaApp/src/app/services/positions.service.ts
@@ -5,7 +5,7 @@ import {HttpClient} from '@angular/common/http';
 @Injectable()
 export class PositionsService {
 
-  url:string='http://10.40.4.64:3000/houses';
+  url:string='http://115.159.104.128:3000/houses';
   constructor(
     private http:HttpClient
   ) {
